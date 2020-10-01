d223c1e97ead9f28fe22fc7d257186d13dbcb964
Julien Veyssier <eneiluj@posteo.net>
2020-10-01 13:55:31 +0200

    register search and widget only if jira app is set in admin settings
    Signed-off-by: Julien Veyssier <eneiluj@posteo.net>

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -17,6 +17,7 @@ use OCP\AppFramework\Bootstrap\IRegistrationContext;
 use OCP\AppFramework\Bootstrap\IBootContext;
 use OCP\AppFramework\Bootstrap\IBootstrap;
 use OCP\Notification\IManager as INotificationManager;
+use OCP\IConfig;
 
 use OCA\Jira\Controller\PageController;
 use OCA\Jira\Dashboard\JiraWidget;
@@ -43,13 +44,19 @@ class Application extends App implements IBootstrap {
 		parent::__construct(self::APP_ID, $urlParams);
 
 		$container = $this->getContainer();
+		$this->container = $container;
 		$manager = $container->query(INotificationManager::class);
 		$manager->registerNotifierService(Notifier::class);
 	}
 
 	public function register(IRegistrationContext $context): void {
-		$context->registerDashboardWidget(JiraWidget::class);
-		$context->registerSearchProvider(JiraSearchProvider::class);
+		$config = $this->container->query(IConfig::class);
+		$clientId = $config->getAppValue(self::APP_ID, 'client_id', '');
+		$clientSecret = $config->getAppValue(self::APP_ID, 'client_secret', '');
+		if ($clientId !== '' && $clientSecret !== '') {
+		    $context->registerDashboardWidget(JiraWidget::class);
+			$context->registerSearchProvider(JiraSearchProvider::class);
+		}
 	}
 
 	public function boot(IBootContext $context): void {
